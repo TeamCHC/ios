21da4323317e429ca2c1bae2486587c41eabadcc
cknutson442 <cknutson@smu.edu>
2014-02-05 18:44:46 -0600

    add switch page
    Conflicts:
    	assignment1/assignment1/Base.lproj/Main_iPhone.storyboard

---
+++ assignment1/assignment1/SwitchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SwitchViewController.h
+//  assignment1
+//
+//  Created by CONNER KNUTSON on 2/5/14.
+//  Copyright (c) 2014 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwitchViewController : UIViewController
+
+@end
