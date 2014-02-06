3ce09619650a17d8f354c0bbb70c263b1148ac8e
cknutson442 <cknutson@smu.edu>
2014-02-06 12:05:49 -0600

    picker stuff

--- assignment1/assignment1/SwitchViewController.m
+++ assignment1/assignment1/SwitchViewController.m
@@ -9,10 +9,11 @@
 #import "SwitchViewController.h"
 
 
-@interface SwitchViewController ()<UIPickerViewDelegate, UIPickerViewDataSource>
+@interface SwitchViewController ()<UIPickerViewDelegate,UIPickerViewDataSource>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIPickerView *photoPicker;
 @property (strong, nonatomic) NSArray *pickerOptions;
+@property (weak, nonatomic) IBOutlet UIImageView *backView;
 
 
 @end
@@ -25,25 +26,26 @@
         self.imageView.image = nil;
 }
 
-- (IBAction)pickerAction:(UIPickerView *)sender2 {
-    
-}
 
-#pragma mark -
-#pragma mark PickerView DataSource
-
-- (NSInteger)numberOfComponentsInPickerView:
-(UIPickerView *)photoPicker
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
     return 1;
 }
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent: (NSInteger)component
+{
+    return 3;
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.photoPicker.delegate = self;
-    _pickerOptions = @[@"Pony",@"SMU"];
+    self.pickerOptions  = [[NSArray alloc]         initWithObjects:@"Blue",@"Red",@"None", nil];
 
 }
 
@@ -51,6 +53,29 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row   forComponent:(NSInteger)component
+{
+    return [self.pickerOptions objectAtIndex:row];
+}
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row   inComponent:(NSInteger)component
+{
+    
+    if(row == 0)
+        self.backView.image = [UIImage imageNamed:@"bluerect"];
+    else if(row == 1)
+        self.backView.image = [UIImage imageNamed:@"redrect"];
+    else
+        self.backView.image = nil;
+    
+    /*switch(row)
+    {
+        case 0:
+            self.backView.image = [UIImage imageNamed:@"bluerect"];
+        case 1:
+            self.backView.image = nil;
+    }*/
 }
 
 @end
