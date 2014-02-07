73a1397031672111b033b9ad20f73c83f58ad91e
cknutson442 <cknutson@smu.edu>
2014-02-06 18:52:35 -0600

    slider programmatically

--- assignment1/assignment1/SliderViewController.m
+++ assignment1/assignment1/SliderViewController.m
@@ -12,6 +12,8 @@
 @property (weak, nonatomic) IBOutlet UISlider *slide;
 - (IBAction)sliderValueChanged:(id)sender;
 @property (weak, nonatomic) IBOutlet UILabel *slideValue;
+@property (weak, nonatomic) IBOutlet UILabel *slideValue2;
+@property (strong, nonatomic) IBOutlet UISlider *slide2;
 
 @end
 
@@ -30,7 +32,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    CGRect myFrame = CGRectMake(20.0f, 200.0f, 115.0f, 25.0f);
+    self.slide2 = [[UISlider alloc] initWithFrame:myFrame];
+    self.slide2.minimumValue = 0.0f;
+    self.slide2.maximumValue = 10.0f;
+    self.slide2.value = 0.0f;
+    [self.slide2 setContinuous:false];
+    [self.slide2 addTarget:self
+                      action:@selector(sliderChange:)
+            forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.slide2];
+    
 }
+
+- (void) sliderChange:(UISlider *)paramSender{
+    _slideValue2.text = [NSString stringWithFormat:@"%d", (int)paramSender.value];
+}
+
+/*- (void)sliderChange:(id)send
+{
+    float value = [send floatValue];
+    _slideValue2.text = [NSString stringWithFormat:@"%d", (int)value];
+}*/
 
 - (void)didReceiveMemoryWarning
 {
