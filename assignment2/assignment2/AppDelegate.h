b6b87b713393e14c1cc67cff42b47c7a4ffd1519
hhouston <hhouston@smu.edu>
2014-02-12 16:55:14 -0600

    a2

---
+++ assignment2/assignment2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  assignment2
+//
+//  Created by Hunter Houston on 2/12/14.
+//  Copyright (c) 2014 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ assignment2/assignment2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  assignment2
+//
+//  Created by Hunter Houston on 2/12/14.
+//  Copyright (c) 2014 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
