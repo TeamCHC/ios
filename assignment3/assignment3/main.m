61bb9b7c8b3030bd3e8e1fe52550dbf650abe826
hhouston <hhouston@smu.edu>
2014-02-24 15:46:41 -0600

    Initial Commit

---
+++ assignment3/assignment3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  assignment3
+//
+//  Created by Hunter Houston on 2/24/14.
+//  Copyright (c) 2014 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
